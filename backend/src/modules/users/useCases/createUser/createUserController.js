13cca1732ee7f15d4e344bbceb3690bb4b748dd4
Lucas <lucas>
2023-05-16 22:09:42 -0300

    optimizing user

--- backend/src/modules/users/useCases/createUser/createUserController.js
+++ backend/src/modules/users/useCases/createUser/createUserController.js
@@ -2,18 +2,30 @@ const CreateUserUseCase = require("./createUserUseCase");
 
 class CreateUserController {
   async handle(req, res) {
-    const { name, email, address, pessoal_number } = req.body;
+    try {
+      const { name, email, password, address, pessoal_number } = req.body;
 
-    const createUserUseCase = new CreateUserUseCase();
+      const createUserUseCase = new CreateUserUseCase();
 
-    const user = await createUserUseCase.execute({
-      name,
-      email,
-      address,
-      pessoal_number,
-    });
+      const user = await createUserUseCase.execute({
+        name,
+        email,
+        password,
+        address,
+        pessoal_number,
+      });
 
-    return res.status(201).json(user);
+      const userResponse = {
+        id: user.id,
+        name: user.name,
+        address: user.address,
+        pessoal_number: user.pessoal_number,
+      };
+
+      return res.status(201).json(userResponse);
+    } catch (error) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
   }
 }
 
--- backend/src/modules/users/useCases/createUser/createUserUseCase.js
+++ backend/src/modules/users/useCases/createUser/createUserUseCase.js
@@ -1,21 +1,32 @@
 const prisma = require("../../../../database/prismaClient");
+const bcrypt = require("bcryptjs");
+const AppError = require("../../../../utils/errors/appError");
 
 class CreateUserUseCase {
-  async execute({ name, email, address, pessoal_number }) {
-    const user = await prisma.users
-      .create({
+  async execute({ name, email, password, address, pessoal_number }) {
+    const user = await prisma.users.findFirst({
+      where: {
+        OR: [{ email }, { pessoal_number }],
+      },
+    });
+
+    if (!user) {
+      const hashPassword = await bcrypt.hash(password, 5);
+
+      const newUser = await prisma.users.create({
         data: {
           name,
           email,
+          password: hashPassword,
           address,
           pessoal_number,
         },
-      })
-      .catch((error) => {
-        console.log(error);
       });
 
-    return user;
+      return newUser;
+    } else {
+      throw new AppError("Email or Number already in use!!", 409);
+    }
   }
 }
 
